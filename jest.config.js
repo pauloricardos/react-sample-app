ee765c83165afeef1ddeec2f23e5c56439518c61
Paulo <paulorsscontato@hotmail.com>
2021-11-03 22:56:07 -0300

    :adhesive_bandage: ident jest config format

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  testPathIgnorePatterns: ["/node_modules/"],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/tests/__mocks__/styleMock.js',
-    '\\.(css|scss)$': '<rootDir>/src/tests/__mocks__/styleMock.js',
-  },
-  setupFilesAfterEnv: [
-    "<rootDir>/src/tests/setupTests.js"
-  ],
-  transform: {
-    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest"
-  },
-  testEnvironment: "jsdom"
+	testPathIgnorePatterns: ['/node_modules/'],
+	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/src/tests/__mocks__/styleMock.js',
+		'\\.(css|scss)$': '<rootDir>/src/tests/__mocks__/styleMock.js',
+	},
+	setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.js'],
+	transform: {
+		'^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
+	},
+	testEnvironment: 'jsdom',
 };
